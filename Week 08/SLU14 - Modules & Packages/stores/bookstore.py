29b31d0c619b3d90efa5a5bf574b224ea41ea8ee
Ana B. Rodrigues <ana.b.a.r.24111@gmail.com>
2023-05-29 22:40:38 +0100

    Exercises for Week 08

--- Week 08/SLU14 - Modules & Packages/stores/bookstore.py
+++ Week 08/SLU14 - Modules & Packages/stores/bookstore.py
@@ -2,28 +2,18 @@ class Book:
     def __init__(self, title, author, price):
         self.title = str(title)
         self.author = str(author)
-        self.price = float(price)
+        self.price = price
         
     def get_book_info(self):
-        return "Title: " + self.title + ", Author: " + self.author + ", Price: " + self.price + "."
+        print("Title: " + str(self.title) + ", Author: " + str(self.author) + ", Price: " + str(self.price))
+        return "Title: " + str(self.title) + ", Author: " + str(self.author) + ", Price: " + str(self.price)
 
-    def get_total_price(self, book_list):
-        total_price = 0
     
-        for book in book_list:
-            total_price += self.price
-        print(total_price)
+def get_total_price(book_list):
+    total_price = 0
+
+    for book in book_list:
+        total_price += book[2]
+    return total_price
     
-    description = "this is a module named bookstore"
-    
-if __name__ == "__main__":
-    Book = [
-        ("To Kill a Mockingbird", "Harper Lee", 2),
-        ("The Great Gatsby", "F. Scott Fitzgerald", 5),
-        ("Pride and Prejudice", "Jane Austen", 7),
-        ("The Catcher in the Rye", "J.D. Salinger", 6),
-        ("1984", "George Orwell", 15)
-    ]
-    
-    print(get_total_price())
-    print(get_book_info())
+description = "this is a module named bookstore"
